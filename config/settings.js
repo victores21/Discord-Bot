45092f4394b5666f6f39c5f3255485465bfcc45f
Victor Escalona <victorescalonag@gmail.com>
2020-01-07 16:17:27 -0500

    Changed config file

---
+++ config/settings.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+const config = {
+  webUrl: "Url to scrap",
+  channelMessageStart: "Discord channel id to start the bot",
+  channelMessageToSend: "discord channel id to alert ",
+  botToken: "bot token"
+};
+
+module.exports = { config };
